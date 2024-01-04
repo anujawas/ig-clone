51b8b1e978e751d7c94dd866433f2dc8303d7362
anujawas <awasthianuj394@gmail.com>
2024-01-04 23:28:24 +0530

    Typescipt config

--- App.js
+++ App.js
@@ -3,13 +3,9 @@ import { View, Text } from "react-native";
 export default function App() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-center items-center bg-black"
     >
-      <Text>Universal React with Expo</Text>
+      <Text className="text-white">Universal React with Expo</Text>
     </View>
   );
 }
