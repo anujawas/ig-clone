785135badc1e521789ade45cd804d69809cf5f41
anujawas <awasthianuj394@gmail.com>
2024-01-06 00:49:38 +0530

    Home Header complete and Moved inside Home directory

---
+++ src/components/Home/Header.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Image style={styles.logo} source={require('../../assets/images/ig-transaparent-white.png')} />
+            </TouchableOpacity>
+
+            <View style={styles.iconContainer}>
+                <TouchableOpacity>
+                    <Image style={styles.icon} source={require('../../assets/images/icons8-add-new-50.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image style={styles.icon} source={require('../../assets/images/icons8-heart-50.png')} />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>
+                            11
+                        </Text>
+                    </View>
+                    <Image style={styles.icon} source={require('../../assets/images/icons8-messenger-50.png')} />
+                </TouchableOpacity>
+
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20
+    },
+    logo: {
+        width: 100,
+        height: 50,
+        resizeMode: 'contain'
+    },
+    iconContainer: {
+        flexDirection: 'row'
+
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        resizeMode: 'contain',
+    },
+    unreadBadge: {
+        backgroundColor: '#ff3250',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100
+    },
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600'
+    }
+})
+
+
+export default Header
