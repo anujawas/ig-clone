08c7d4ef1009bda9039deaa079f2e789269bc7a1
anujawas <awasthianuj394@gmail.com>
2024-01-06 04:24:27 +0530

    Post Component started

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../components/Home/Header'
+import Stories from '../components/Home/Stories'
+import Post from '../components/Home/Post'
+
+
+import { POSTS } from '../../assets/data/Posts'
 
 const HomeScreen = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Header />
+            <Stories />
+            <ScrollView>
+                {POSTS.map((post, index) => (<Post post={post} key={index} />))}
+            </ScrollView>
         </SafeAreaView>
     )
 }
