76ba51a673526cbfa2fead72684dee19593843a1
anujawas <awasthianuj394@gmail.com>
2024-01-07 01:15:06 +0530

    Post footer icons

--- assets/data/PostFooterIcons.js
+++ assets/data/PostFooterIcons.js
@@ -1,20 +1,20 @@
 export const POST_FOOTER_ICONS = [
     {
         name: "Like",
-        imageUrl: "https://icons8.com/icon/87/heart",
-        likeImageUrl: "../../../assets/images/icon8-heart-50-red.png"
+        imageUrl: "https://img.icons8.com/ios/50/ffffff/like--v1.png",
+        likeImageUrl: "https://img.icons8.com/ios-filled/50/FF0000/like--v1.png"
     },
     {
         name: "comment",
-        imageUrl: "icon8-comment-50.png"
+        imageUrl: "https://img.icons8.com/ios/50/FFFFFF/speech-bubble--v1.png"
     },
     {
         name: "share",
-        imageUrl: "icon8-share-64.png"
+        imageUrl: "https://img.icons8.com/external-creatype-outline-colourcreatype/64/FFFFFF/external-share-user-interface-creatype-outline-colourcreatype.png"
     },
     {
         name: "save",
-        imageUrl: "icon8-save-96.png",
-        savedImg: 'icon8-save-96-filled.png'
+        imageUrl: "https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/FFFFFF/external-bookmark-social-media-ui-tanah-basah-basic-outline-tanah-basah.png",
+        savedImg: 'https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/FFFFFF/external-bookmark-social-media-ui-tanah-basah-basic-outline-tanah-basah.png'
     }
 ]
