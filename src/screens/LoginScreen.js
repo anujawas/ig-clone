9b3a9fdd2fc5460a56f0a8d0bb3e77c6d1d276e8
anujawas <awasthianuj394@gmail.com>
2024-01-08 02:33:25 +0530

    Created Login Screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,40 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import LoginForm from '../components/loginScreen/LoginForm'
+const IG_LOGO = 'https://www.shawspaving.co.uk/wp-content/uploads/2019/01/instagram-font-logo-white-png-350x133.png'
+
+const LoginScreen = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image
+                    source={{
+                        uri: IG_LOGO
+                    }}
+                    style={{
+                        width: 200,
+                        height: 100,
+                        resizeMode: 'contain'
+                    }}
+                />
+            </View>
+            <LoginForm />
+        </SafeAreaView>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        paddingTop: 155,
+        paddingHorizontal: 12
+    },
+    logoContainer: {
+        marginTop: 60,
+        alignItems: 'center'
+    }
+})
