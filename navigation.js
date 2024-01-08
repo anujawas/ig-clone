b291ef1b04079adb36f38686e04a5fef0a2e1d09
anujawas <awasthianuj394@gmail.com>
2024-01-09 02:02:26 +0530

    navigation to Login and Signup added

--- navigation.js
+++ navigation.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './src/screens/HomeScreen';
 import NewPostScreen from './src/screens/NewPostScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 const Stack = createStackNavigator();
 
 const screenOptions = {
@@ -14,11 +15,11 @@ const screenOptions = {
 
 const SignedInStack = () => (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home' screenOptions={screenOptions}>
+        <Stack.Navigator initialRouteName='Login' screenOptions={screenOptions}>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="NewPost" component={NewPostScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
-
+            <Stack.Screen name="SignUp" component={SignUpScreen} />
         </Stack.Navigator>
     </NavigationContainer>
 )
