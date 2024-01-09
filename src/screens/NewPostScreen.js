618c3dbb77bc02359b31d7976a0afb1a7aebd755
anujawas <awasthianuj394@gmail.com>
2024-01-10 00:04:11 +0530

    Added local file uploader to new post scree,

--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AddNewPost from '../components/NewPost/AddNewPost'
+import UploadScreen from './NewPostUpload'
 const NewPostScreen = ({ navigation }) => {
     return (
         <SafeAreaView className="bg-black flex-1">
-            <AddNewPost navigation={navigation} />
+            {/* <AddNewPost navigation={navigation} /> */}
+            <UploadScreen />
         </SafeAreaView>
     )
 }
