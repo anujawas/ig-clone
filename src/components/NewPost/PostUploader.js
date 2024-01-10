481bee76ec4bdc036f56bc886873120af89c7854
anujawas <awasthianuj394@gmail.com>
2024-01-10 05:43:41 +0530

    Post Uploader component complete and rerouted

--- src/components/NewPost/PostUploader.js
+++ src/components/NewPost/PostUploader.js
@@ -1,79 +1,87 @@
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
+import { Button, Image, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
 import * as Yup from 'yup'
 
 import { Formik } from 'formik'
 import { Divider } from 'react-native-elements'
 
-import validUrl from 'valid-url'
 
-const PLACEHOLDER_IMAGE = 'https://placehold.co/600x400/png'
 
 const UploadPostScheme = Yup.object().shape({
-    imgUrl: Yup.string().url().required('Post Image Url is required'),
     caption: Yup.string().max(2200, 'Caption has a maximum characters limit.')
 })
 
-const PostUploader = ({ navigation }) => {
-    const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMAGE)
+const PostUploader = ({ navigation, selectedImage }) => {
     return (
         <Formik
-            initialValues={{ imgUrl: '', caption: '' }}
+            initialValues={{ caption: '' }}
             onSubmit={(values) => {
                 console.log(values)
                 console.log("You have successfully uploaded a post");
-                navigation.goBack()
+                navigation.navigate('Home')
             }}
             validationSchema={UploadPostScheme}
             validateOnMount={true}
-
         >
-            {({ handleBlur, handleChange, handleSubmit, values, errors, isValid }) => <>
-                <View className="m-4 justify-between flex-row">
-                    <Image source={{ uri: validUrl.isUri(thumbnailUrl) ? thumbnailUrl : PLACEHOLDER_IMAGE }}
-                        style={{ width: 100, height: 100 }} />
-                    <View className="flex-1 ml-3">
-                        <TextInput
-                            placeholder='Write a caption'
-                            placeholderTextColor={'gray'}
-                            multiline={true}
-                            style={{
-                                color: 'white',
-                                fontSize: 20,
-                            }}
-                            onChangeText={handleChange('caption')}
-                            onBlur={handleBlur('caption')}
-                            value={values.caption}
-                        />
+
+            {({ handleBlur, handleChange, handleSubmit, values }) => <>
+                <View className="items-center flex-1 justify-between">
+                    <View className="flex-1 items-center justify-around w-full bottom-4" style={{ marginHorizontal: 10 }}>
+                        <Image source={{ uri: `${selectedImage}` }}
+                            style={{ width: 300, height: 300, resizeMode: 'contain' }} />
+
+                        <View className=" w-full h-[20%]">
+                            <Divider width={0.2}
+                                orientation='vertical' className="mb-2" />
+                            <TextInput
+                                placeholder='Write a caption'
+                                placeholderTextColor={'gray'}
+                                multiline={true}
+                                style={{
+                                    color: 'white',
+                                    fontSize: 20,
+                                    paddingHorizontal: 5,
+                                    borderColor: 'red',
+                                    backgroundColor: '#1c1c1e',
+                                    width: '100%',
+                                    height: '50%'
+
+                                }}
+                                onChangeText={handleChange('caption')}
+                                onBlur={handleBlur('caption')}
+                                value={values.caption}
+                            />
+                        </View>
                     </View>
+                    <Pressable style={styles.submitButton} onPress={handleSubmit}>
+                        <Text className="text-white font-semibold
+                text-md
+                "
+                        >
+                            Share
+                        </Text>
+                    </Pressable>
                 </View>
-                <Divider width={0.2}
-                    orientation='vertical' />
-                <TextInput
-
-                    onChange={(e) => setThumbnailUrl(e.nativeEvent.text)}
-                    placeholder='Enter Image Url'
-                    placeholderTextColor={'gray'}
-                    style={{
-                        color: 'white',
-                        fontSize: 18,
-                    }}
-                    onChangeText={handleChange('imgUrl')}
-                    onBlur={handleBlur('imgUrl')}
-                    value={values.imgUrl}
-                />
-                {errors.imgUrl &&
-                    <Text style={{ color: 'red', fontSize: 10 }}>
-                        {errors.imgUrl}
-                    </Text>
-                }
-                <Button title='Share' onPress={handleSubmit} disabled={!isValid} />
-
             </>}
-        </Formik>
+        </Formik >
+
+
     )
 }
 
 export default PostUploader
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    submitButton: {
+        backgroundColor: '#1DA1F2',
+        alignItems: "center",
+        justifyContent: 'center',
+        fontSize: 20,
+        borderRadius: 8,
+        shadowOpacity: 0.3,
+        width: '80%',
+        height: 50,
+        bottom: 20,
+        shadowOffset: { width: 4, height: 4 },
+    }
+})
