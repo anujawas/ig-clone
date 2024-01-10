904ea0e42039e5c58698be96223fccb1c74e1f7b
anujawas <awasthianuj394@gmail.com>
2024-01-10 05:44:50 +0530

    Uploading Image state and NewPost Upload Route added

--- navigation.js
+++ navigation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
@@ -7,21 +7,30 @@ import HomeScreen from './src/screens/HomeScreen';
 import NewPostScreen from './src/screens/NewPostScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import UploadPostScreen from './src/screens/UploadPostScreen';
 const Stack = createStackNavigator();
 
 const screenOptions = {
     headerShown: false
 }
 
-const SignedInStack = () => (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home' screenOptions={screenOptions}>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="NewPost" component={NewPostScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
-        </Stack.Navigator>
-    </NavigationContainer>
-)
+const SignedInStack = () => {
+    const [selectedImage, setSelectedImage] = useState(null)
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home' screenOptions={screenOptions}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="NewPost">
+                    {props => <NewPostScreen {...props} setSelectedImage={setSelectedImage} />}
+                </Stack.Screen>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+                <Stack.Screen name="Upload">
+                    {props => <UploadPostScreen {...props} selectedImage={selectedImage} />}
+                </Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
 
 export default SignedInStack
