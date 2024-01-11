0763ea89552a35677018b03c1f9af1ff8866f6ea
anujawas <awasthianuj394@gmail.com>
2024-01-12 04:42:30 +0530

    Post image update

--- assets/data/Posts.js
+++ assets/data/Posts.js
@@ -1,7 +1,7 @@
 import { Users } from './Users'
 export const POSTS = [
     {
-        imageUrl: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202401/rohit-sharma--virat-kohli-16210057-16x9.jpg?VersionId=7OIdApCT1V0WGQiTITCb2SBoz5BOYLMc",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/rn-ig-clone-e753f.appspot.com/o/postImage%2Fe2cb1db4-eb68-4271-90b4-ff50b8b3ec6d.jpeg?alt=media&token=7ce2b7e6-9ced-4061-9cfd-c85088db0fc2",
         user: Users[0].username,
         likes: 54001,
         caption: "Re-contextualized real-time software Hello world. Last Year was fun",
