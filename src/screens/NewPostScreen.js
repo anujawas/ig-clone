18240ab73b4beb532d3b98181e2897fe49d6b58c
anujawas <awasthianuj394@gmail.com>
2024-01-14 03:41:35 +0530

    Added flip camera feature

--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AddNewPost from '../components/NewPost/AddNewPost'
-const NewPostScreen = ({ navigation, setSelectedImage }) => {
+const NewPostScreen = ({ navigation, selectedImage, setSelectedImage }) => {
     return (
         <SafeAreaView className="bg-black flex-1">
-            <AddNewPost navigation={navigation} setSelectedImage={setSelectedImage} />
+            <AddNewPost navigation={navigation} selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
         </SafeAreaView>
     )
 }
