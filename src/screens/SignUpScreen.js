9cb7abf254c74c4ed0de84c3f2acb8ad57704e1c
anujawas <awasthianuj394@gmail.com>
2024-01-14 03:40:56 +0530

    moved ig logo to signup form

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -4,24 +4,11 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import SignUpForm from '../components/Signup/SignupForm'
 import Loader from '../components/global/Loader'
 import { useLoading } from '../../LoadingContext'
-const IG_LOGO = 'https://www.shawspaving.co.uk/wp-content/uploads/2019/01/instagram-font-logo-white-png-350x133.png'
 
 const SignUpScreen = ({ navigation }) => {
     const { loading } = useLoading()
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.logoContainer}>
-                <Image
-                    source={{
-                        uri: IG_LOGO
-                    }}
-                    style={{
-                        width: 200,
-                        height: 100,
-                        resizeMode: 'contain'
-                    }}
-                />
-            </View>
             <SignUpForm navigation={navigation} />
             <Loader visible={loading} />
         </SafeAreaView>
@@ -34,11 +21,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'black',
-        paddingTop: 100,
+        paddingTop: 20,
         paddingHorizontal: 12
-    },
-    logoContainer: {
-        marginTop: 60,
-        alignItems: 'center'
     }
 })
