665e339f829735103bb9ec3fc9f1d4b44165c6f2
anujawas <awasthianuj394@gmail.com>
2024-01-15 02:48:25 +0530

    Created search Screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchBar from '../components/Search/SearchBar'
+import BottomTabs from '../components/Home/BottomTabs'
+import useActiveTab from '../hooks/FooterState'
+
+
+const SearchScreen = ({ navigation }) => {
+    const { activeTab } = useActiveTab('Search')
+    return (
+        <SafeAreaView style={styles.container}>
+            <SearchBar />
+            <BottomTabs navigation={navigation} activeTab={activeTab} />
+        </SafeAreaView>
+    )
+}
+
+export default SearchScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#080404'
+    }
+
+})
