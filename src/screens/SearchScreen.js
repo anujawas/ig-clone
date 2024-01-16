c06e10d5f44de9dc68e2377c42c50502762afb3d
anujawas <awasthianuj394@gmail.com>
2024-01-17 01:46:20 +0530

    imported random image array and passed it to Explore grid

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SearchBar from '../components/Search/SearchBar'
 import BottomTabs from '../components/Home/BottomTabs'
-import useActiveTab from '../hooks/useFooterState'
 import ExploreGrid from '../components/Search/ExploreGrid'
+
+import useActiveTab from '../hooks/useFooterState'
+import { firebase, db } from '../../firebase'
 
 
 const SearchScreen = ({ navigation }) => {
     const { activeTab } = useActiveTab('Search')
+    const [pictures, setPictures] = useState([]);
+    useEffect(() => {
+        if (firebase.auth().currentUser !== null) {
+            db.collectionGroup('posts').onSnapshot(snapshot => {
+                setPictures(snapshot.docs.map(doc => doc.data().imageUrl));
+            })
+        }
+
+    }, [])
     return (
         <SafeAreaView style={styles.container}>
             <SearchBar />
-            <ExploreGrid />
+            <ExploreGrid data={pictures} />
             <BottomTabs navigation={navigation} activeTab={activeTab} />
         </SafeAreaView>
     )
